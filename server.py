27d661e583db1ab19db81ffc55eabafcb4dd2c2f
admyavuzs <sayilgan.yavuz@gmail.com>
2020-09-12 16:36:38 +0000

    exposing prometheus metrics

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import http.server
 import socketserver
 from http import HTTPStatus
-
+from prometheus_client import start_http_server
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -15,5 +15,10 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b'I am healty')
 
-httpd = socketserver.TCPServer(('', 8080), Handler)
-httpd.serve_forever()
+#httpd = socketserver.TCPServer(('', 8080), Handler)
+#httpd.serve_forever()
+
+if __name__ == "__main__":
+    start_http_server(8081)
+    server = http.server.HTTPServer(('', 8080), Handler)
+    server.serve_forever()
